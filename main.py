2b43dc5534a7e34dc30ff004d99e6bb8febc4f99
mishiiyumi <koyeababto@gmail.com>
2026-05-22 01:20:36 +0800

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-print("LTO Information Management System")
-print("Database successfully connected.")
-print("Driver Management")
-print("Vehicle Registration")
-print("Traffic Violation Monitoring")
+from db import get_connection
+
+try:
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    print("=== LTO Information Management System ===")
+    print("Database Connected Successfully")
+
+    cursor.execute("SHOW TABLES")
+
+    print("\nTables inside JimlethDB:")
+    for table in cursor:
+        print("-", table[0])
+
+    conn.close()
+
+except Exception as e:
+    print("Connection Error:", e)
